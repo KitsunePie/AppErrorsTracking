f7c975e6de3c65727c6eff6b4e2ce363f1f44dee
fankesyooni <qzmmcn@163.com>
2022-10-03 02:34:16 +0800

    Added thread pool function

---
+++ app/src/main/java/com/fankes/apperrorstracking/utils/factory/ThreadPooFactory.kt
@@ -0,0 +1,46 @@
+/*
+ * AppErrorsTracking - Added more features to app's crash dialog, fixed custom rom deleted dialog, the best experience to Android developer.
+ * Copyright (C) 2019-2022 Fankes Studio(qzmmcn@163.com)
+ * https://github.com/KitsunePie/AppErrorsTracking
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ *
+ * This file is Created by fankes on 2022/10/3.
+ */
+package com.fankes.apperrorstracking.utils.factory
+
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+/**
+ * 创建当前线程池服务
+ * @return [ExecutorService]
+ */
+private val currentThreadPool get() = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
+
+/**
+ * 创建并启动新的临时线程池
+ *
+ * 等待 [block] 执行完成并自动释放
+ * @param block 方法块
+ */
+fun newThread(block: () -> Unit) {
+    currentThreadPool.apply {
+        execute {
+            block()
+            shutdown()
+        }
+    }
+}
