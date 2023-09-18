b0a6c71300f9386bbb2702c933d40bb495fd3676
fankesyooni <qzmmcn@163.com>
2023-09-19 05:02:15 +0800

    chore: migrate build script from groovy to kts
    - using SweetDependency, SweetProperty
    - merge singing key file configs to properties
    - update gradle and dependencies

---
+++ demo-app/build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    autowire(libs.plugins.android.application)
+    autowire(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = property.project.demo.app.packageName
+    compileSdk = property.project.android.compileSdk
+    ndkVersion = property.project.android.ndk.version
+
+    signingConfigs {
+        create("universal") {
+            keyAlias = property.project.demo.app.signing.keyAlias
+            keyPassword = property.project.demo.app.signing.keyPassword
+            storeFile = rootProject.file(property.project.demo.app.signing.storeFilePath)
+            storePassword = property.project.demo.app.signing.storePassword
+            enableV1Signing = true
+            enableV2Signing = true
+        }
+    }
+    defaultConfig {
+        applicationId = property.project.demo.app.packageName
+        minSdk = property.project.android.minSdk
+        targetSdk = property.project.android.targetSdk
+        versionName = property.project.demo.app.versionName
+        versionCode = property.project.demo.app.versionCode
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        all { signingConfig = signingConfigs.getByName("universal") }
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path("src/main/cpp/CMakeLists.txt")
+            version = property.project.android.cmake.version
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+        freeCompilerArgs = listOf(
+            "-Xno-param-assertions",
+            "-Xno-call-assertions",
+            "-Xno-receiver-assertions"
+        )
+    }
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+    lint { checkReleaseBuilds = false }
+    androidResources.additionalParameters += listOf("--allow-reserved-package-id", "--package-id", "0x37")
+}
+
+androidComponents {
+    onVariants(selector().all()) {
+        it.outputs.forEach { output ->
+            val currentType = it.buildType
+            val currentSuffix = property.github.ci.commit.id.let { suffix -> if (suffix.isNotBlank()) "-$suffix" else "" }
+            val currentVersion = "${output.versionName.get()}$currentSuffix(${output.versionCode.get()})"
+            if (output is com.android.build.api.variant.impl.VariantOutputImpl)
+                output.outputFileName.set("${property.project.name}-v$currentVersion-$currentType.apk")
+        }
+    }
+}
+
+dependencies {
+    implementation(com.fankes.projectpromote.project.promote)
+    implementation(com.highcapable.yukireflection.api)
+    implementation(androidx.core.core.ktx)
+    implementation(androidx.appcompat.appcompat)
+    implementation(com.google.android.material.material)
+    testImplementation(junit.junit)
+    androidTestImplementation(androidx.test.ext.junit)
+    androidTestImplementation(androidx.test.espresso.espresso.core)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+plugins {
+    id("com.highcapable.sweetdependency") version "1.0.1"
+    id("com.highcapable.sweetproperty") version "1.0.2"
+}
+sweetProperty {
+    global {
+        all {
+            permanentKeyValues(
+                "GITHUB_CI_COMMIT_ID" to "",
+                "APP_CENTER_SECRET" to ""
+            )
+            generateFrom(ROOT_PROJECT, SYSTEM_ENV)
+        }
+        sourcesCode {
+            propertiesFileNames(".secret/secret.properties")
+            includeKeys("GITHUB_CI_COMMIT_ID", "APP_CENTER_SECRET")
+        }
+    }
+    rootProject { all { isEnable = false } }
+}
+rootProject.name = "AppErrorsTracking"
+include(":module-app", ":demo-app")
