4fff1d7c170e680d22e57c3a7dc344a879ce210d
fankesyooni <qzmmcn@163.com>
2023-09-19 05:13:25 +0800

    docs: optimize comments

--- module-app/src/main/java/com/fankes/apperrorstracking/utils/tool/GithubReleaseTool.kt
+++ module-app/src/main/java/com/fankes/apperrorstracking/utils/tool/GithubReleaseTool.kt
@@ -17,7 +17,7 @@
  * and eula along with this software.  If not, see
  * <https://www.gnu.org/licenses/>
  *
- * This file is Created by fankes on 2023/1/23.
+ * This file is created by fankes on 2023/1/23.
  */
 package com.fankes.apperrorstracking.utils.tool
 
